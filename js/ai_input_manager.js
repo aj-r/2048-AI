37f1a70d33bed7079f2ebbe59ba2a8f82e70e579
AJ Richardson <aj_r@shaw.ca>
2014-04-07 16:42:24 -0400

    Implemented RNG AI

--- js/ai_input_manager.js
+++ js/ai_input_manager.js
@@ -15,6 +15,12 @@ function AIInputManager() {
   this.listen();
 }
 
+AIMode = { SMART: 0, RNG: 1 };
+
+AIInputManager.prototype.mode = AIMode.RNG;
+AIInputManager.prototype.moveTime = 150; // milliseconds
+AIInputManager.prototype.game = null;
+
 AIInputManager.prototype.on = function (event, callback) {
   if (!this.events[event]) {
     this.events[event] = [];
@@ -34,79 +40,62 @@ AIInputManager.prototype.emit = function (event, data) {
 AIInputManager.prototype.listen = function () {
   var self = this;
 
-  var map = {
-    38: 0, // Up
-    39: 1, // Right
-    40: 2, // Down
-    37: 3, // Left
-    75: 0, // Vim up
-    76: 1, // Vim right
-    74: 2, // Vim down
-    72: 3, // Vim left
-    87: 0, // W
-    68: 1, // D
-    83: 2, // S
-    65: 3  // A
-  };
-
   // Respond to button presses
   this.bindButtonPress(".retry-button", this.restart);
-  //this.bindButtonPress(".restart-button", this.restart);
   this.bindButtonPress(".keep-playing-button", this.keepPlaying);
 
-  // Respond to swipe events
-  var touchStartClientX, touchStartClientY;
-  var gameContainer = document.getElementsByClassName("game-container")[0];
-
-  gameContainer.addEventListener(this.eventTouchstart, function (event) {
-    if ((!window.navigator.msPointerEnabled && event.touches.length > 1) ||
-        event.targetTouches > 1) {
-      return; // Ignore if touching with more than 1 finger
-    }
-
-    if (window.navigator.msPointerEnabled) {
-      touchStartClientX = event.pageX;
-      touchStartClientY = event.pageY;
-    } else {
-      touchStartClientX = event.touches[0].clientX;
-      touchStartClientY = event.touches[0].clientY;
-    }
-
-    event.preventDefault();
-  });
-
-  gameContainer.addEventListener(this.eventTouchmove, function (event) {
-    event.preventDefault();
-  });
-
-  gameContainer.addEventListener(this.eventTouchend, function (event) {
-    if ((!window.navigator.msPointerEnabled && event.touches.length > 0) ||
-        event.targetTouches > 0) {
-      return; // Ignore if still touching with one or more fingers
-    }
-
-    var touchEndClientX, touchEndClientY;
-
-    if (window.navigator.msPointerEnabled) {
-      touchEndClientX = event.pageX;
-      touchEndClientY = event.pageY;
-    } else {
-      touchEndClientX = event.changedTouches[0].clientX;
-      touchEndClientY = event.changedTouches[0].clientY;
-    }
-
-    var dx = touchEndClientX - touchStartClientX;
-    var absDx = Math.abs(dx);
-
-    var dy = touchEndClientY - touchStartClientY;
-    var absDy = Math.abs(dy);
-
-    if (Math.max(absDx, absDy) > 10) {
-      // (right : left) : (down : up)
-      self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
-    }
-  });
+  // Start running the AI.
+  // Wait for a specified time interval before making each move
+  // so the AI is watchable
+  this.aiID = setInterval(this.nextMove.bind(this), this.moveTime);
 };
+
+AIInputManager.prototype.nextMove = function() {
+  var self = this;
+  var move = 0;
+  switch (this.mode) {
+    case AIMode.SMART:
+      move = this.nextMoveSmart();
+      break;
+    case AIMode.RNG:
+      move = this.nextMoveRNG();
+      break;
+  }
+  this.emit("move", move);
+
+  // If the game is over, do a longer wait and start again.
+  if (this.game.over) {
+    clearInterval(this.aiID);
+    setTimeout(function() {
+      self.emit("restart");
+      self.aiID = setInterval(self.nextMove.bind(self), self.moveTime);
+      }, 5000);
+  }
+}
+
+AIInputManager.prototype.nextMoveRNG = function() {
+  // Move in a random direction.
+  // TODO: determine which directions are valid
+  return Math.floor(Math.random() * 4);
+}
+
+AIInputManager.prototype.nextMoveSmart = function() {
+  // Determine the best move given the current game state
+  // Use a couple of stragtegies:
+  // 1. Goals:
+  //   - Determine the main goal given the current board state
+  //     (e.g. if the highest number is 64, build it up to 128)
+  //   - Determine sub-goals that must happen to achieve that goal
+  // 2. Planning ahead
+  //   - in some cases, the AI should plan ahead a certain number of
+  //     moves to determine the effect of each possible move it can make.
+  //     If it sees that a certain move will put the board in a bad state
+  //     (i.e. forced to push the wrong direction), then it will avoid that
+  //     move. Alternatively, if it sees a sequence of moves that will 
+  //     accomplish a goal, it will do those moves.
+  var move = 0;
+  return move;
+}
 
 AIInputManager.prototype.restart = function (event) {
   event.preventDefault();
